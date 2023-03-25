004a7a83e6e280281163754b00ed98da6f3bace7
Maaya <yamauchimaaya@gmail.com>
2023-03-25 17:14:20 -0500

    created initialplanets in seed and now showing Mars

--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,34 @@
 const { client } = require("./client");
 const bcrypt = require("bcrypt");
+const {createPlanet} = require("./index");
+
+async function createInitialPlanets() {
+  try {
+    console.log("Starting to create planets...");
+
+    const Mars = await createPlanet({
+      planet_name: "Mars",
+      name_origin: "Mars is named after the Roman God of war.",
+      radius: "3390",
+      orbit: "687 Earth Days",
+      rotation: "24.6 Hours",
+      sun_distance: "228 million km",
+      planet_type: true, 
+      moon_num: 2,
+      fact_one: "Mars is colloquially known as the Red Planet due to its red appearance. It gets its red color from the oxidation of iron in its soil. ",
+      fact_two: "Mars has the largest volcano in our solar system called Olympus Mons. ",
+      fact_three: "Mars used to have liquid water. This is evidenced by the existence of ancient river valleys and floodplains, deltas, lakebeds, and sedimentary rock and mineral deposits. Today, all of Mars's water is frozen at its polar ice caps. "
+    })
+
+    console.log(Mars);
+
+    console.log("Finished creating planets!");
+
+  } catch (error) {
+    console.error("Error creating planets!");
+    throw error;
+  }
+}
 
 async function dropTables() {
   try {
@@ -24,17 +53,17 @@ async function createTables(){
 
     CREATE TABLE planets(
       id SERIAL PRIMARY KEY,
-      role planet_name NOT NULL,
+      planet_name VARCHAR(255),
       name_origin VARCHAR(255),
-      fact_one VARCHAR(255),
-      fact_two VARCHAR(255),
-      fact_three VARCHAR(255),
       radius DECIMAL(10,2),
       orbit VARCHAR(255),
       rotation VARCHAR(255),
       sun_distance VARCHAR(255),
       planet_type_gas BOOLEAN DEFAULT false,
-      moon_num INT NOT NULL DEFAULT(0)
+      moon_num INT NOT NULL DEFAULT(0),
+      fact_one VARCHAR(255),
+      fact_two VARCHAR(255),
+      fact_three VARCHAR(255)
     );
 
     CREATE TABLE moons (
@@ -57,6 +86,7 @@ async function buildingDB() {
     client.connect();
     await dropTables();
     await createTables();
+    await createInitialPlanets();
   } catch (error) {
     console.log("error during building");
     throw error;
