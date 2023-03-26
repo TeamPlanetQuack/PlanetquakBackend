5b5e525096bc15cbb30e94fe05fd1ad23cb698ef
Jessy <japarker0421@gmail.com>
2023-03-25 19:54:11 -0600

    got db functions set for moons and tested in seed

--- db/moons.js
+++ db/moons.js
@@ -18,7 +18,67 @@ async function createMoon({
     }
   }
 
+  async function getAllMoons(){
+    try {
+      const {rows: moons}= await client.query(`
+      SELECT *
+      FROM moons
+      `);
+      return moons;
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async function getMoonById(moon_id){
+    try {
+      const{rows:[moon]}= await client.query(`
+      SELECT *
+      FROM moons
+      WHERE id=${moon_id}
+      `);
+      if(!moon){
+        return null;
+      }
+      return moon;
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async function getMoonByName(moonName){
+    try {
+      const{rows:moon}= await client.query(`
+      SELECT *
+      FROM moons
+      WHERE moon_name=$1
+      `, [moonName]) ;
+      if(!moon){
+        return null;
+      }
+      return moon;
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async function getMoonsByPlanetId(planet_id){
+    try {
+      const {rows: moons}= await client.query(`
+      SELECT *
+      FROM moons
+      WHERE planet_id=${planet_id}
+      `);
+      return moons;
+    } catch (error) {
+      throw error
+    }
+  }
 
 module.exports={
-createMoon
+createMoon,
+getAllMoons,
+getMoonById,
+getMoonsByPlanetId,
+getMoonByName
 }
