14772f24fca1f22c9eadd5271969aa170e2d0d0d
Jessy <japarker0421@gmail.com>
2023-03-30 15:18:03 -0600

    more questions

--- db/quizdata.js
+++ db/quizdata.js
@@ -1,49 +1,196 @@
-const quizQuestions=[
-  {question:"What is is the planet closest to the sun?",
-  correct_answer:"Mercury",
-  incorrect_answers:["Mars", "Venus", "Earth"]
-},
-{question:"Why doesn’t Mercury have an atmosphere?",
-  correct_answer:"It get’s blown away by solar wind",
-  incorrect_answers:["It’s too hot", "It's too cold", "No oxygen"]
-},
-{question:"How fast does Mercury orbit the sun?",
-  correct_answer:"47 km/seconds",
-  incorrect_answers:["47 km/minute", "47 km/hour", "47 km/day"]
-},
-{question:"What is Mercury’s radius?",
-  correct_answer:"2,440km",
-  incorrect_answers:["6,050km", "10,489km", "1,567km"]
-},
-{question:"What planet is named after the Roman god of love and beauty?",
-  correct_answer:"Venus",
-  incorrect_answers:["Neptune", "Mars", "Uranus"]
-},
-{question:"What is Venus's radius?",
-  correct_answer:"6,050km",
-  incorrect_answers:["2,440km", "10,489km", "1,567km"]
-},
-{question:"What is the hottest planet?",
-  correct_answer:"Venus",
-  incorrect_answers:["Mercury", "Mars", "Uranus"]
-},
-{question:"What is Mercury's surface covered in?",
-  correct_answer:"Craters",
-  incorrect_answers:["Gas", "Water", "Volcanoes"]
-},
-{question:"What is Venus's surface covered in?",
-  correct_answer:"Volcanoes",
-  incorrect_answers:["Gas", "Water", "Craters"]
-},
-{question:"What is the only planet that rotates counterclockwise?",
-  correct_answer:"Venus",
-  incorrect_answers:["Earth", "Saturn", "Jupiter"]
-},
-{question:"What is the distance from Earth to the Sun?",
-  correct_answer:"150 million km",
-  incorrect_answers:["110 million km", "402 million km", "210 million km"]
-},
-]
+const quizQuestions = [
+  {
+    question: "What is is the planet closest to the sun?",
+    correct_answer: "Mercury",
+    incorrect_answers: ["Mars", "Venus", "Earth"],
+  },
+  {
+    question: "Why doesn’t Mercury have an atmosphere?",
+    correct_answer: "It get’s blown away by solar wind",
+    incorrect_answers: ["It’s too hot", "It's too cold", "No oxygen"],
+  },
+  {
+    question: "How fast does Mercury orbit the sun?",
+    correct_answer: "47 km/seconds",
+    incorrect_answers: ["47 km/minute", "47 km/hour", "47 km/day"],
+  },
+  {
+    question: "What is Mercury’s radius?",
+    correct_answer: "2,440km",
+    incorrect_answers: ["6,050km", "10,489km", "1,567km"],
+  },
+  {
+    question: "What planet is named after the Roman god of love and beauty?",
+    correct_answer: "Venus",
+    incorrect_answers: ["Neptune", "Mars", "Uranus"],
+  },
+  {
+    question: "What is Venus's radius?",
+    correct_answer: "6,050km",
+    incorrect_answers: ["2,440km", "10,489km", "1,567km"],
+  },
+  {
+    question: "What is the hottest planet?",
+    correct_answer: "Venus",
+    incorrect_answers: ["Mercury", "Mars", "Uranus"],
+  },
+  {
+    question: "What is Mercury's surface covered in?",
+    correct_answer: "Craters",
+    incorrect_answers: ["Gas", "Water", "Volcanoes"],
+  },
+  {
+    question: "What is Venus's surface covered in?",
+    correct_answer: "Volcanoes",
+    incorrect_answers: ["Gas", "Water", "Craters"],
+  },
+  {
+    question: "What is the only planet that rotates counterclockwise?",
+    correct_answer: "Venus",
+    incorrect_answers: ["Earth", "Saturn", "Jupiter"],
+  },
+  {
+    question: "What is the distance from Earth to the Sun?",
+    correct_answer: "150 million km",
+    incorrect_answers: ["110 million km", "402 million km", "210 million km"],
+  },
+  {
+    question:
+      "Earth is tilted, which is what allows us to have seasons. What is that tilt?",
+    correct_answer: "23.4 degrees",
+    incorrect_answers: ["50 degrees", "32.4 degrees", "10.7 degrees"],
+  },
+  {
+    question: "What is the 'habitable zone?'",
+    correct_answer:
+      "The perfect distance from the sun in which the temperatures are just right for liquid water to exist on the surface",
+    incorrect_answers: [
+      "The habitable zone is the area around a star where alien life forms are most likely to exist.",
+      "The habitable zone is the region in space where spacecraft are least likely to encounter space debris.",
+      "The habitable zone is the distance from a planet's core where the temperature is suitable for the formation of precious gems.",
+    ],
+  },
+  {
+    question: "What planet is named after the Roman God of war?",
+    correct_answer: "Mars",
+    incorrect_answers: ["Jupiter", "Saturn", "Uranus"],
+  },
+  {
+    question: "Why is Mars red?",
+    correct_answer:
+      "It gets its red color from the oxidation of iron in its soil.",
+    incorrect_answers: [
+      "Mars is red because it's covered in a layer of rust that formed over millions of years.",
+      "Mars is red because it's made of a special type of rock that only exists on the planet, which is naturally red in color.",
+      "Mars is red because it's the remnants of a giant tomato that was destroyed in a cosmic disaster eons ago.",
+    ],
+  },
+  {
+    question: "How many moons does Mars have?",
+    correct_answer: "2",
+    incorrect_answers: ["1", "12", "5"],
+  },
+  {
+    question: "What planet is named after the Roman King of the Gods?",
+    correct_answer: "Jupiter",
+    incorrect_answers: ["Mars", "Saturn", "Uranus"],
+  },
+  {
+    question: "Which is the largest planet?",
+    correct_answer: "Jupiter",
+    incorrect_answers: ["Earth", "Saturn", "Neptune"],
+  },
+  {
+    question: "What is the fastest rotating planet",
+    correct_answer: "Jupiter",
+    incorrect_answers: ["Earth", "Mars", "Mercury"],
+  },
+  {
+    question:
+      "What planet is named after the Roman God of agriculture and wealth?",
+    correct_answer: "Saturn",
+    incorrect_answers: ["Venus", "Mars", "Mercury"],
+  },
+  {
+    question: "What planet has the lowest density of all the 8 major planets?",
+    correct_answer: "Saturn",
+    incorrect_answers: ["Venus", "Uranus", "Earth"],
+  },
+  {
+    question: "How many moons does Jupiter have?",
+    correct_answer: "80",
+    incorrect_answers: ["25", "32", "4"],
+  },
+  {
+    question: "How many moons does Saturn have?",
+    correct_answer: "83",
+    incorrect_answers: ["1", "12", "52"],
+  },
+  {
+    question: "How long does it take Saturn to orbit?",
+    correct_answer: "10,585 earth days",
+    incorrect_answers: ["5,625 earth days", "12 earth days", "612 earth days"],
+  },
+  {
+    question: "What planet is named after the Greed God of the sky?",
+    correct_answer: "Uranus",
+    incorrect_answers: ["Mercury", "Neptune", "Saturn"],
+  },
+  {
+    question: "What axis does Uranus spin at?",
+    correct_answer: "90 degrees",
+    incorrect_answers: ["30 degrees", "10.2 degrees", "104 degrees"],
+  },
+  {
+    question: "Which planet appears to be spinning on its side?",
+    correct_answer: "Uranus",
+    incorrect_answers: ["Jupiter", "Neptune", "Venus"],
+  },
+  {
+    question: "Why is Uranus blue?",
+    correct_answer:
+      "Uranus gets its blue color from methane gas in the atmosphere.",
+    incorrect_answers: [
+      "The blue color of Uranus is a reflection of the planet's mood, which is usually calm and peaceful.",
+      "The blue color of Uranus is caused by the reflection of the ocean on the planet's surface.",
+      "Uranus is blue because it is made of ice.",
+    ],
+  },
+  {
+    question: "What planet is named after the Roman God of the sea?",
+    correct_answer: "Neptune",
+    incorrect_answers: ["Uranus", "Mars", "Mercury"],
+  },
+  {
+    question: "What is the windest planet in our solar system?",
+    correct_answer: "Neptune",
+    incorrect_answers: ["Jupiter", "Earth", "Mercury"],
+  },
+  {
+    question:
+      "Due to it's orbit this planet is sometimes further away then the dwarf planet Pluto",
+    correct_answer: "Neptune",
+    incorrect_answers: ["Saturn", "Uranus", "Earth"],
+  },
+  {
+    question:
+      "What planet has the most moons?",
+    correct_answer: "Saturn",
+    incorrect_answers: ["Jupiter", "Uranus", "Earth"],
+  },
+  {
+    question:
+      "What is the biggest moon in out solar System",
+    correct_answer: "Ganymede",
+    incorrect_answers: ["Titan", "Deimos", "Io"],
+  },
+  {
+    question:
+      "What planets' moons (besides earth) are not named after Greek mythology? ",
+    correct_answer: "Uranus",
+    incorrect_answers: ["Jupiter", "Mars", "Neptune"],
+  },
+];
 
 
 module.exports = {
